4b10795541fee8332f5583c12e2a79237c77c5ed
KeanuDecleene <keanudecleene124@gmail.com>
2024-04-30 19:01:25 +1200

    q2 done

--- ex3_q2.s
+++ ex3_q2.s
@@ -2,3 +2,23 @@
 .text
 
 main:
+
+addi $4, $0, '*' #sets replacement character
+lw $11, 0x70003($0) #gets status register value
+andi $11, $11, 0x1 #checks transmit data
+beqz $11, main #if not ready loop until it is
+lw $3, 0x70001($0) #gets character from serial port 1
+sw $3, 0x70000($0) #sends character to serial port 1
+
+sgei $3, $3, 'a' #if character is greater or equal to 'a'then change char
+beqz $3, changeChar
+slei $3, $3, 'z' #if character is less than or equal to 'z' then change char
+beqz $3, changeChar
+
+j main
+
+
+changeChar: #if char is not lowercase change to replacement character
+ sw $4, 0x70000($0)
+ j main
+
