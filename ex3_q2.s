0923f600ce601f5855cd51d273d26a3dcee13908
KeanuDecleene <keanudecleene124@gmail.com>
2024-04-30 19:17:36 +1200

    comment change

--- ex3_q2.s
+++ ex3_q2.s
@@ -10,13 +10,12 @@ beqz $11, main #if not ready loop until it is
 lw $3, 0x70001($0) #gets character from serial port 1
 sw $3, 0x70000($0) #sends character to serial port 1
 
-sgei $3, $3, 'a' #if character is greater or equal to 'a'then change char
+sgei $3, $3, 'a' #if character is greater or equal to 'a'then dont change char
 beqz $3, changeChar
-slei $3, $3, 'z' #if character is less than or equal to 'z' then change char
+slei $3, $3, 'z' #if character is less than or equal to 'z' then dont change char
 beqz $3, changeChar
 
 j main
-
 
 changeChar: #if char is not lowercase change to replacement character
  sw $4, 0x70000($0)
